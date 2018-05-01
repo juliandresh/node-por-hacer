e0cbadd24060bab17f83624979954a5e0f482ad7
Julian <jhernandeza@gmail.com>
2018-04-30 19:25:13 -0500

    04- Bases de Node: Primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Aplicación de comandos
+
+Este es el ejercicio del curso
+
+Recuerdar la instalación de los paquetes de node
+
+
+```
+npm install
+```
---
+++ app.js
@@ -0,0 +1,36 @@
+const colors = require('colors');
+const argv = require('./conf/yargs').argv;
+
+const porHacer = require('./por-hacer/por-hacer')
+
+let comando = argv._[0];
+
+switch (comando) {
+    
+    case 'crear':
+        let tarea = porHacer.crear( argv.descripcion );        
+        break;
+    
+        case 'listar':
+        let listado = porHacer.getListado();
+        console.log('======================'.green);
+        for (let tarea of listado) {
+            console.log(tarea.descripcion);
+            console.log('Estado: ', tarea.completado);
+            console.log('======================'.green);
+        }
+        break;
+    
+        case 'actualizar':
+            let actualizado = porHacer.actualizar(argv.descripcion, argv.completado);      
+            console.log(actualizado);
+        break;
+    
+        case 'borrar':            
+            let borrado = porHacer.borrar(argv.descripcion);      
+            console.log(borrado);
+        break;
+    
+        default:
+        console.log('Comando no es reconocido');
+}
